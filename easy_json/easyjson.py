ffd4d5a97ed28636f466bc6d0e64afb62bd7b974
Geoff Riley <geoffr-dev@adaso.com>
2021-11-29 08:15:18 +0000

    Solution for easyjson Not exactly optimum, but it works!

--- easy_json/easyjson.py
+++ easy_json/easyjson.py
@@ -1,24 +1,30 @@
 import json
-from collections.abc import Mapping
+from typing import Union
+
+ellipsis = type(Ellipsis)
 
 
-class MyJSONObject(Mapping):
-    def __init__(self, json_dict: dict) -> None:
+class MyJSONObject:
+    def __init__(self, json_dict: Union[list, dict]) -> None:
         self._json = json_dict
+        super().__init__()
 
-    def __getitem__(self, ndx):
-        # print(type(ndx), ndx)
+    def __getitem__(self, ndx: Union[ellipsis, int, slice, str]):
         if ndx is Ellipsis:
-            return self._json.values
+            return MyJSONObject(self._json)
         if isinstance(ndx, tuple):
             return MyJSONObject({x: self._json[x] for x in ndx})
-        return self._json[ndx]
+        if isinstance(self._json, list):
+            try:
+                value = MyJSONObject([x[ndx] for x in self._json])
+            except KeyError:
+                value = self._json[ndx]
+        else:
+            value = self._json[ndx]
+        return value if not isinstance(value, list) else MyJSONObject(value)
 
     def __iter__(self):
         return iter(self._json)
-
-    def __len__(self) -> int:
-        return len(self._json)
 
     def __getattr__(self, ndx):
         return self[ndx]
@@ -26,6 +32,12 @@ class MyJSONObject(Mapping):
     def __repr__(self) -> str:
         return repr(self._json)
 
+    def __eq__(self, __o: object) -> bool:
+        return (self._json) == (__o)
+
+    def keys(self) -> list:
+        return self._json.keys()
+
 
 def parse(js_data: str):
-    return json.loads(js_data, object_hook=MyJSONObject)
+    return MyJSONObject(json.loads(js_data, object_hook=MyJSONObject))
